5dff20275bf234d14cb9f81b2ddf92b3762dc038
just-another-mammal <ee384318@gmail.com>
2026-01-17 05:07:06 -0500

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   main.cpp 	new file:   main.exe 	new file:   preview.jpg 	new file:   sfml-graphics-3.dll 	new file:   sfml-system-3.dll 	new file:   sfml-window-3.dll

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <Windows.h>
+#include <array>
+#include <cmath>
+using namespace std;
+
+//int screen_x = GetSystemMetrics(0);
+//int screen_y = GetSystemMetrics(1);
+int screen_x = GetSystemMetrics(SM_CXSCREEN);
+int screen_y = GetSystemMetrics(SM_CYSCREEN);
+const size_t img_num = 32;
+
+array<sf::Image, img_num> imgProcessing()
+{
+    
+    array<sf::Image, img_num> splitImg;
+    splitImg.fill(sf::Image({99, 64}, sf::Color::Black));
+    sf::Image tiles2("preview.jpg");
+
+    array<int, 2> imgNum2d = {8, 4};
+    array<int, 2> startingPos = {34, 443};
+    int i = 0;
+
+    for (int y = 0; y < imgNum2d[1]; y++)
+    {
+        for (int x = 0; x < imgNum2d[0]; x++)
+        {   
+            if (!splitImg[i].copy(tiles2, {0,0}, sf::IntRect(sf::Vector2i({startingPos[0]+(x*110), startingPos[1]+(y*75)}), sf::Vector2i({100, 65}))))
+            {
+                cout << x << ", " << y << ", " << "i: " << i << "not working\n";
+            }
+                
+            i++;
+        }
+    }
+    
+
+    return splitImg;
+}
+
+
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window", sf::Style::Default);
+    array<sf::Image, img_num> tileImg = imgProcessing();
+    //array<sf::Texture, 28> tilesTexture;
+    //tilesTexture.fill(sf::Texture({99, 64}));
+    sf::Texture tilesTexture({99, 64});
+    /*
+    for (int i = 0; i < 28; i++)
+    {
+        sf::Texture tilesTexture[i](tileImg[i], {99, 64});
+        tilesTexture[i].update(tileImg[i]);
+
+    }
+    */
+    sf::Sprite tilesSprite(tilesTexture);
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std::optional event = window.pollEvent())
+        {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+            else if (event->is<sf::Event::Resized>())
+            {
+                sf::View view(sf::FloatRect({0.f,0.f}, sf::Vector2f(window.getSize())));
+                window.setView(view);
+                //std::cout << window.getView().getSize().x << ", " << window.getView().getSize().y;
+            }
+        }
+        
+        
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        for (int i = 0; i < img_num; i++)
+        {
+            tilesTexture.update(tileImg[i]);
+            window.draw(tilesSprite);
+            tilesSprite.setPosition({(i%6)*100.f, floor(i/6.f)*65.f});
+        }
+
+        // draw everything here...
+        // window.draw(...);
+
+        // end the current frame
+        window.display();
+    }
+}
